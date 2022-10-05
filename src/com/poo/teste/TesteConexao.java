30092a4fda07fbb8d8dcda2195166041ab53258f
Jusley Souza <jusleysouza31@gmail.com>
2022-10-05 19:22:05 -0300

    Creation of the database connection test class

---
+++ src/com/poo/teste/TesteConexao.java
@@ -0,0 +1,10 @@
+package com.poo.teste;
+
+import com.poo.banco.Conexao;
+
+public class TesteConexao {
+
+	public static void main(String[] args) {
+		System.out.println(Conexao.conectaBD());
+	}
+}
